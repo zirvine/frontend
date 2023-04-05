09a43a9a3c0ee4b3ec8d56226251d71384bd7237
Zachary Irvine <irvinezachary@gmail.com>
2023-04-05 11:33:28 -0600

    Mission14

---
+++ src/movie/movie.ts
@@ -0,0 +1,11 @@
+export type Movie = {
+  movieId: number;
+  category: string;
+  title: string;
+  year: number;
+  director: string;
+  rating: string;
+  edited: string;
+  lentTo: string;
+  notes: string;
+};
--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,9 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import data from '../MovieData.json';
+import { Movie } from '../movie/movie';
 
 const MDS = data.MovieData;
 
 function Movies() {
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const rsp = await fetch('https://localhost:4000/movie');
+      const temp = await rsp.json();
+      setMovieData(temp);
+    };
+    fetchMovie();
+  }, []);
+
   return (
     <>
       <br></br>
@@ -17,23 +29,29 @@ function Movies() {
           <table className="table">
             <thead>
               <tr>
+                <th>Movie ID</th>
+                <th>Category</th>
                 <th>Title</th>
                 <th>Year</th>
                 <th>Director</th>
                 <th>Rating</th>
-                <th>Category</th>
                 <th>Edited</th>
+                <th>Lent To</th>
+                <th>Notes</th>
               </tr>
             </thead>
             <tbody>
-              {MDS.map((m) => (
+              {movieData.map((m) => (
                 <tr>
-                  <td>{m.Title}</td>
-                  <td>{m.Year}</td>
-                  <td>{m.Director}</td>
-                  <td>{m.Rating}</td>
-                  <td>{m.Category}</td>
-                  <td>{m.Edited}</td>
+                  <td>{m.movieId}</td>
+                  <td>{m.category}</td>
+                  <td>{m.title}</td>
+                  <td>{m.year}</td>
+                  <td>{m.director}</td>
+                  <td>{m.rating}</td>
+                  <td>{m.edited}</td>
+                  <td>{m.lentTo}</td>
+                  <td>{m.notes}</td>
                 </tr>
               ))}
             </tbody>
